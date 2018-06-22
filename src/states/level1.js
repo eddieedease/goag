c2e304d8312ced933800683eeb568f802c88f01d
Eddie Maas <eddie@edease.nl>
2018-06-22 14:30:22 +0200

    loopingenemies

--- src/states/level1.js
+++ src/states/level1.js
@@ -69,14 +69,14 @@ class Level1 extends Phaser.State {
 
       this.enemies = this.game.add.physicsGroup();
 
-      for (var i = 0; i < 9; i++) {
+      for (var i = 0; i < 15; i++) {
         var rand = this.game.rnd.between(100, 900);
         // var enemy = this.game.add.sprite(rand, 30, 'wizardsheet', 'wizard_2_walk_001.png');
 
         var enemy = this.enemies.create(this.game.world.randomX, 30, 'wizardsheet');
         enemy.scale.setTo(1.5, 1.5);
         enemy.anchor.setTo(0.5, 0.5);
-  
+
         // animation sets
         enemy.animations.add('walk', Phaser.Animation.generateFrameNames('wizard_2_walk_', 1, 8, '.png', 3), 10, true, false);
         enemy.animations.add('run', Phaser.Animation.generateFrameNames('wizard_2_run_', 1, 8, '.png', 3), 10, true, false);
@@ -86,7 +86,7 @@ class Level1 extends Phaser.State {
         enemy.animations.add('hit', Phaser.Animation.generateFrameNames('wizard_2_hit_', 1, 2, '.png', 3), 10, false, false);
         enemy.animations.add('crouch', Phaser.Animation.generateFrameNames('wizard_2__crouch_', 1, 2, '.png', 3), 10, false, false);
         enemy.animations.add('standing', [0], 20, true);
-  
+
         // facingenemy should be right or left
         this.facingenemy = 'left';
         enemy.scale.x = -1.5;
@@ -100,7 +100,7 @@ class Level1 extends Phaser.State {
       }
 
 
-     
+
     }
 
 
@@ -119,7 +119,7 @@ class Level1 extends Phaser.State {
     // if enemy is alive
     if (this.enemyalive === true) {
       this.enemies.forEach(this.enemyAI, this);
-      
+
     }
 
     this.game.physics.arcade.collide(this.player, this.collisionlayer);
@@ -174,8 +174,24 @@ class Level1 extends Phaser.State {
 
 
   enemyAI(enemy) {
+
+    // reset the position if leftbottom or rightbottom
+    if (enemy.x <= 60 && enemy.y > 680 ) {
+      // check left bottom
+      console.log("hitleftbottom");
+      enemy.y = 30;
+      enemy.x = 500;
+    } 
+
+    // check right bottom
+    if (enemy.x >= 960 && enemy.y > 680) {
+      enemy.y = 30;
+      enemy.x = 500;
+      console.log("hitrightbottom");
+    }
+
     // What side are we facing?
-    if (enemy.scale.x <= 0 ) {
+    if (enemy.scale.x <= 0) {
       enemy.animations.play('walk');
       enemy.body.velocity.x = -250;
 
@@ -188,16 +204,15 @@ class Level1 extends Phaser.State {
     this.game.physics.arcade.collide(this.player, enemy);
     this.game.physics.arcade.collide(this.enemies, enemy);
     this.game.physics.arcade.collide(enemy, this.collisionlayer, this.enemyCollisionHandler, null, this);
-    
+
 
     // add probability to jump with random number
-    var x = Math.floor((Math.random() * 10) + 1);
+    var x = Math.floor((Math.random() * 20) + 1);
     if (x === 9) {
       if (enemy.body.onFloor()) {
         enemy.body.velocity.y = -400;
         enemy.animations.play('jump');
-      } else {
-      }
+      } else {}
     }
   }
 
@@ -205,7 +220,6 @@ class Level1 extends Phaser.State {
   // eneymy tilemap collision handler
   enemyCollisionHandler(spriteThatCollided, tileThatCollided) {
 
-    console.log("doesss something happens?");
     //console.log(spriteThatCollided.body.onFloor());
     // The turning when colliding in objects
     if (spriteThatCollided.body.blocked.left) {
@@ -226,6 +240,8 @@ class Level1 extends Phaser.State {
   render() {
     //this.game.debug.body(this.player);
     // this.game.debug.bodyInfo(this.player, 32, 20);
+    // mouse debugging
+    //this.game.debug.pointer( this.game.input.activePointer );
   }
 
   shutdown() {
